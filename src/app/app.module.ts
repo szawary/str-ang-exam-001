316f1cea0a02af07a46dc0c1bf42ffc98ff0584b
Szabo Laszlo <szabo.laszlo@szawary.eu>
2021-02-15 21:37:06 +0100

    componensek létrehozása, home és heroes komponens routing beállítása

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './common/navigation/navigation.component';
+import { HomeComponent } from './common/home/home.component';
+import { HeroesComponent } from './common/heroes/heroes.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "heroes",
+    component: HeroesComponent
+  },
+  {
+    path: "**",
+    component: HomeComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
