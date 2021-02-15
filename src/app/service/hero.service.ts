01b10bc3cb97bb0c6ad99e830b660d9ca0e8233d
Szabo Laszlo <szabo.laszlo@szawary.eu>
2021-02-15 22:04:24 +0100

    heroes is created

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Hero } from '../model/hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  heroes: Hero[] = [
+  {
+    id: 1,
+    name: "Superman",
+    superPower: "flying",
+    address: "New Your"
+  },
+  {
+    id: 2,
+    name: "Spiderman",
+    superPower: "climbing",
+    address: "Washington"
+  },
+  {
+    id: 3,
+    name: "DarkFater",
+    superPower: "kardvívás",
+    address: "Space",
+  },
+  {
+    id: 4,
+    name: "Octopus",
+    superPower: "bellowing",
+    address: "Under the sea"
+  },
+  {
+    id: 5,
+    name: "White shale",
+    superPower: "biging",
+    address: "Mars"
+  }]
+
 
   constructor() { }
 }
+
+
